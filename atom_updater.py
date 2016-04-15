3a67a88840858033b23f30151c6d4b7713d8a111
Matthew Feickert <mfeickert@gmail.com>
2016-04-14 22:12:34 -0500

    Change call for INSTALLED to reflect change in atom --version output. Change subprocess call syntax for ease of human reading

--- atom_updater.py
+++ atom_updater.py
@@ -5,12 +5,13 @@
 
 import sys
 import subprocess
+import re
 
 if (sys.version_info > (3, 0)):
     # Python 3 code
     import urllib.request
 
-    INSTALLED = subprocess.getoutput('atom --version')[:5]
+    INSTALLED = subprocess.getoutput('atom --version | grep Atom')[-5:]
     URL = "https://github.com/atom/atom/releases/latest"
     LATEST = urllib.request.urlopen(URL).geturl()[-5:]
 
@@ -22,11 +23,11 @@ if (sys.version_info > (3, 0)):
         CHOICE = input("Would you like to update Atom? [Y/n]")
         if (CHOICE == 'Y'):
             print("Updating Atom")
-            subprocess.call(['wget', '-O', 'atom-amd64.deb',
-                             'https://atom.io/download/deb'])
-            subprocess.call(['sudo', 'dpkg', '-i', 'atom-amd64.deb'])
-            subprocess.call(['rm', 'atom-amd64.deb'])
-            INSTALLED = subprocess.getoutput('atom --version')[:5]
+            subprocess.call(
+                'wget -O atom-amd64.deb https://atom.io/download/deb', shell=True)
+            subprocess.call('sudo dpkg -i atom-amd64.deb', shell=True)
+            subprocess.call('rm atom-amd64.deb', shell=True)
+            INSTALLED = subprocess.getoutput('atom --version | grep Atom')[-5:]
             print("Atom has been updated to v{}".format(INSTALLED))
         else:
             print("Exiting")
@@ -34,7 +35,8 @@ else:
     # Python 2 code
     import urllib2
 
-    INSTALLED = subprocess.check_output(['atom', '--version'])[:5]
+    INSTALLED = subprocess.check_output(
+        "atom --version | grep Atom", shell=True)[-6:-1]
     URL = "https://github.com/atom/atom/releases/latest"
     LATEST = urllib2.urlopen(URL).geturl()[-5:]
 
@@ -46,11 +48,12 @@ else:
         CHOICE = raw_input("Would you like to update Atom? [Y/n]")
         if (CHOICE == 'Y'):
             print("Updating Atom")
-            subprocess.call(['wget', '-O', 'atom-amd64.deb',
-                             'https://atom.io/download/deb'])
-            subprocess.call(['sudo', 'dpkg', '-i', 'atom-amd64.deb'])
-            subprocess.call(['rm', 'atom-amd64.deb'])
-            INSTALLED = subprocess.check_output(['atom', '--version'])[:5]
+            subprocess.call(
+                'wget -O atom-amd64.deb https://atom.io/download/deb', shell=True)
+            subprocess.call('sudo dpkg -i atom-amd64.deb', shell=True)
+            subprocess.call('rm atom-amd64.deb', shell=True)
+            INSTALLED = subprocess.check_output(
+                "atom --version | grep Atom", shell=True)[-6:-1]
             print("Atom has been updated to v{}".format(INSTALLED))
         else:
             print("Exiting")
